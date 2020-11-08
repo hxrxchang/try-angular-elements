95ef5ebcf160062438d5b86ba7d5269fa73d0b20
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-11-09 00:33:40 +0900

    hello-world componentを表示させる

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'try-angular-elements';
+
+  showMessage(): void {
+    const hello = document.getElementById('hello');
+    hello.innerHTML = '<app-hello-world></app-hello-world>';
+  }
 }
